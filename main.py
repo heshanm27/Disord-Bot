ae717a47143e7679599aa94fd091dc537481b4ab
B.D.Heshan Madhuranga IT20611910 <39455525+heshanm27@users.noreply.github.com>
2022-07-19 15:22:11 +0530

    update

--- main.py
+++ main.py
@@ -5,19 +5,59 @@ import requests
 import json
 from youtubesearchpython import *
 import os
+import DiscordUtils
 
 
-from help_cog import help_cog
-from music_cog import music_og
+from music_cog import music_cog
 
 intents = discord.Intents.default()
 intents.members = True
-
+music = DiscordUtils.Music()
 bot  = commands.Bot(command_prefix='$',intents=intents)
 
-bot.add_cog(music_cog(bot))
-bot.add_cog(helper_cog(bot))
+bot.add_cog(music_cog(bot,music))
+# bot.add_cog(helper_cog(bot))
 
 
 
-bot.run(os.getenv("BotApiToken"))
+
+
+#make bot leave from voice channel 
+@bot.command(pass_context=True)
+async def leave(ctx):
+    if(ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Doby must leave voice channel !")
+    else:
+        await ctx.send("Doby not in a voice channel")
+
+
+@bot.command(pass_context=True)
+async def ytplay(ctx,url:str):
+    try:
+        video = Video.get(url, mode = ResultMode.json, get_upload_date=True)
+        print(type(video))
+        print(video)
+        thumbnails = video.get('thumbnails') 
+        # json_formated_str = json.dumps(video,indent=2)
+        # print(json_formated_str)
+        embed = discord.Embed(title=video.get('title'),url=video.get('link'))
+        embed.set_author(name=ctx.author.display_name,icon_url=ctx.author.avatar_url)
+        embed.add_field(name='Publish Date',value=video.get('publishDate'),inline=True)
+        embed.add_field(name='View Count',value=video.get('viewCount')['text'],inline=True)
+        for thumb in thumbnails:
+            embed.set_thumbnail(url=thumb['url'])
+
+        await ctx.send(embed=embed)
+    except:
+        await ctx.send('Master Your link is invalid,Please check again')
+
+
+
+@bot.event
+async def on_ready():
+    print("The Bot is now ready for use!")
+    print(".........................")
+
+
+bot.run('OTk4MjE0MDA1Nzg3MjA5NzU4.GGsWiI.UNvdUGLR5OnpeU8VJpJOZhVzp8AEYdCm_E6_24')
--- music_cog.py
+++ music_cog.py
@@ -1,78 +1,43 @@
-import re
+
 import discord
 from discord.ext import commands
-from youtube_dl import YoutubeDL 
-
-
-class music_cog(commands.bot):
-    def __init__(self,bot):
+from youtube_dl import YoutubeDL
+import DiscordUtils
+class music_cog(commands.Cog):
+    def __init__(self, bot,music):
         self.bot = bot
-
+        self.music = music
+        #all the music related stuff
         self.is_playing = False
         self.is_paused = False
 
+        # 2d array containing [song, channel]
         self.music_queue = []
-        self.YDL_options = {'format':'bestaudio','no-playlist':'True'}
+        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
         self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
         self.vc = None
 
-
-    def search_yt(self,item):
-        with YoutubeDL(self.YDL_options) as ydl:
-            try:
-                info = ydl.extract_info("ytsearch:%s" % item,download=False)['entries'][0]
-                print(info)
-            except Exception:
-                return False
-        return {'source':info['formats'[0]['urls']],'title':info['title']}
-
-    def play_next(self):
-        if len(self.music_queue) >0:
-            self.is_playing = True
-
-            m_url = self.music_queue[0][0]['source']
-
-            self.music_queue.pop(0)
-            self.vc.play(discord.FFmpegPCMAudio(m_url,**self.FFMPEG_OPTIONS),after=lambda e:self.play_next())
+     #searching the item on youtube
+    @commands.command(pass_context=True)
+    async def join(self,ctx):
+        if(ctx.author.voice):
+            channel =ctx.message.author.voice.channel
+            await channel.connect()
         else:
-            self.is_playing = False
-
-    async def play_music(self,ctx):
-        if len(self.music_queue) > 0:
-            self.is_playing =True
-            m_url = self.music_queue[0][0]['source']
-
-            if self.vc == None  :
-                await ctx.send("Doby can't connet to the voice channel" )
-                return
-            else:
-                await self.vc.move_to(self.music_queue[0][1])
-            
-            self.music_queue.pop(0)
-
-            self.vc.play(discord.FFmpegPCMAudio(m_url,**self.FFMPEG_OPTIONS),after=lambda e:self.play_next())
-
-        else:
-            self.is_playing =False
-
-    @commands.command(name="play",aliases=["p","playing"],help="play the selected song from youtube")
-    async def play(self,ctx,*args):
-        query = " ".join(args)
-
-        voice_channel = ctx.author.voice.channel
-
-        if voice_channel is None:
             await ctx.send("Master not in a voice channel,Doby can't join")
-        elif self.is_paused:
-            self.vc.resume()
-        else:
-            song = self.search_yt(query)
-            if type(song) == type(True):
-                await ctx.send("Doby ould not downlaod the song,try a different keyword")
-            else:
-                await ctx.send('Song added to the song queue')
-                self.music_queue.append([song,voice_channel])
 
-                if self.is_playing == False:
-                    await self.play_music(ctx)
+
+    @commands.command()
+    async def play(self,ctx, *, url):
+        await self.join(ctx)
+        player = self.music.get_player(guild_id=ctx.guild.id)
+        if not player:
+            player = self.music.create_player(ctx, ffmpeg_error_betterfix=True)
+        if not ctx.voice_client.is_playing():
+            await player.queue(url, search=True)
+            song = await player.play()
+            await ctx.send(f"Playing {song.name}")
+        else:
+            song = await player.queue(url, search=True)
+            await ctx.send(f"Queued {song.name}")
